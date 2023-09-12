e7eff72c7699e9c5aee05ea7bdb3cd6f255460e6
Saydiakhror Botirov <t19b0007s@jdu.uz>
2023-09-13 02:00:45 +0500

    root changed to display username DEMO, checking required!

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,77 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import logo from "./logo.png";
 import { Link, useNavigate } from "react-router-dom";
-
-// Import the font awesome icons
-import {
-  faList,
-  faCalendarDay,
-  faUserPlus,
-  faRotateRight,
-  faThumbsUp,
-  faBoxOpen,
-  faTrashCan,
-  faCircleChevronRight,
-  faRightFromBracket,
-  faMoon,
-  faSun,
-  faFolderPlus,
-  faChevronLeft,
-  faChevronRight,
-  faRightToBracket,
-} from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays, faUserSecret, faRightToBracket, faChevronRight, faList, faCalendarDay, faRotateRight, faThumbsUp, faBoxOpen, faRightFromBracket, faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 import CurrentDate from "../Date";
 import { Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { faCalendarDays } from "@fortawesome/free-solid-svg-icons/faCalendarDays";
-import { faUserSecret } from "@fortawesome/free-solid-svg-icons/faUserSecret";
 
-// Define the props type for the component
-type DashboardSideBarMenuProps = {
-  // You can add any props you need here
-};
+type DashboardSideBarMenuProps = {};
 
-// Create a component that renders the JSX
 function DashboardSideBarMenu(props: DashboardSideBarMenuProps) {
   const navigate = useNavigate();
-  const CheckUser = () => {
-    const token = localStorage.getItem("token");
-    let config = {
-      method: "get",
-      maxBodyLength: Infinity,
-      url: "http://" + window.location.hostname + ":3001/api/checkuser",
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
+  const [userData, setUserData] = useState(null);
 
-    axios
-      .request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch((error) => {
-        console.log(error);
-        // navigate("/login");
-      });
-  };
+  async function fetchUserData() {
+    try {
+      const token = localStorage.getItem("token");
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: "http://" + window.location.hostname + ":3001/api/checkuser",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      };
 
-  CheckUser();
-  type ClickEvent = React.MouseEvent<HTMLDivElement>;
-  function handleClick(event: ClickEvent) {
+      const response = await axios.request(config);
+
+      // If the request is successful, update the ActiveUser in local storage
+      const userData = {
+        UserName: response.data.UserName,
+        UserID: response.data.UserID,
+      };
+      localStorage.setItem("ActiveUser", JSON.stringify(userData));
+
+      // Set the user data in state
+      setUserData(userData);
+    } catch (error) {
+      console.log(error);
+
+      // Set ActiveUser to "Guest" and null for UserID
+      const guestUser = {
+        UserName: "ゲスト",
+        UserID: null,
+      };
+      localStorage.setItem("ActiveUser", JSON.stringify(guestUser));
+
+      // Set the guest user data in state
+      setUserData(guestUser);
+    }
+  }
+
+  useEffect(() => {
+    // Call fetchUserData when the component mounts
+    fetchUserData();
+  }, []);
+
+  function handleClick(event) {
     event.preventDefault();
-    const sidebar: HTMLElement = document.querySelector(
-      ".sidebar"
-    ) as HTMLElement;
+    const sidebar = document.querySelector(".sidebar");
     sidebar.classList.toggle("close");
   }
 
-  // Function to handle screen resize
   function handleResize() {
-    const sidebar: HTMLElement | null = document.querySelector(".sidebar");
+    const sidebar = document.querySelector(".sidebar");
     const screenWidth = window.innerWidth;
 
     if (sidebar) {
@@ -85,11 +78,7 @@ function DashboardSideBarMenu(props: DashboardSideBarMenuProps) {
   handleResize();
 
   const handleLogout = () => {
-    // Perform logout actions, e.g., clear authentication token or user data
-    // For example, if you're using localStorage for the token:
     localStorage.removeItem("token");
-
-    // Redirect the user to the logout page (assuming "/LogOut" is your logout page)
     window.location.reload();
   };
 
@@ -98,14 +87,16 @@ function DashboardSideBarMenu(props: DashboardSideBarMenuProps) {
       <div className="bottom-content">
         <li className="LogStatus">
           <Link to="/login">
-            <div  className="icon" />
-            <div className="UserData"><FontAwesomeIcon icon={faUserSecret} />ゲスト</div>
+            <div className="icon" />
+            <div className="UserData">
+              <FontAwesomeIcon icon={faUserSecret} />
+              ゲスト
+            </div>
           </Link>
         </li>
         <li className="LogStatus">
           <Link to="/login">
-          <FontAwesomeIcon icon={faRightToBracket} className="icon" />
-            {/* <FontAwesomeIcon icon={faUserPlus}  /> */}
+            <FontAwesomeIcon icon={faRightToBracket} className="icon" />
             <span className="text nav-text">ログイン</span>
           </Link>
         </li>
@@ -116,20 +107,21 @@ function DashboardSideBarMenu(props: DashboardSideBarMenuProps) {
       <div className="bottom-content">
         <li className="LogStatus">
           <Link to="/login">
-            <div  className="icon" />
-            <div className="UserData" >Botirov Saydiakhror</div>
+            <div className="icon" />
+            <div className="UserData">
+              {userData ? userData.UserName : ""}
+            </div>
           </Link>
         </li>
         <li className="LogStatus">
           <Link to="/" onClick={handleLogout}>
-            
             <FontAwesomeIcon icon={faRightFromBracket} className="icon" />
             <span className="text nav-text">ログアウト</span>
           </Link>
         </li>
       </div>
     );
-    // console.log
+
     return localStorage.hasOwnProperty("token") ? logOut : createAccount;
   }
 
@@ -167,12 +159,6 @@ function DashboardSideBarMenu(props: DashboardSideBarMenuProps) {
 
         <div className="menu-bar">
           <div className="menu">
-            {/* Uncomment this section if you want to add a search box */}
-            {/*<li className="search-box">
-<FontAwesomeIcon icon={faSearch} className="icon" />
-<input type="search" placeholder="Search..." />
-</li>*/}
-            {/* <CurrentDate /> */}
             <ul className="menu-links">
               <li className="nav-link">
                 <Link to={`/`}>
@@ -210,7 +196,6 @@ function DashboardSideBarMenu(props: DashboardSideBarMenuProps) {
                   <span className="text nav-text">未完了のタスク</span>
                 </Link>
               </li>
-
               <li className="nav-link">
                 <Link to="/categories">
                   <FontAwesomeIcon
@@ -218,10 +203,7 @@ function DashboardSideBarMenu(props: DashboardSideBarMenuProps) {
                     className="icon"
                     id="addCategoryIcon"
                   />
-                  <span
-                    className="text nav-text"
-                    id="addCategoryText"
-                  >
+                  <span className="text nav-text" id="addCategoryText">
                     カテゴリー一覧
                   </span>
                 </Link>
@@ -236,4 +218,4 @@ function DashboardSideBarMenu(props: DashboardSideBarMenuProps) {
   );
 }
 
-export default DashboardSideBarMenu; // Export the component
+export default DashboardSideBarMenu;
